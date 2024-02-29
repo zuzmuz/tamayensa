ceeeaecf384706d55da10d916127dbef592f147f
zahersunique <zaher@bandindustries.com>
2024-02-29 21:16:34 +0100

    applied formatting

--- lib/main.dart
+++ lib/main.dart
@@ -2,38 +2,39 @@ import 'package:flutter/material.dart';
 import 'utils/app_colors.dart';
 
 void main() {
-    runApp(MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final List<String> items = List<String>.generate(10000, (i) => 'Item $i');
 
-    final List<String> items = List<String>.generate(10000, (i) => 'Item $i');
+  MyApp({super.key});
 
-    MyApp({super.key});
-
-    // This widget is the root of your application.
-    @override
-    Widget build(BuildContext context) {
-        return MaterialApp(
-            home: Scaffold(
-                backgroundColor: AppColor.background,
-                body: ListView.builder(itemCount: items.length, itemBuilder: (context, index) {
-                    return ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: AppColor.line,
-                            foregroundColor: AppColor.foreground,
-                            shadowColor: AppColor.foreground,
-                            elevation: 5,
-                            textStyle: const TextStyle(fontSize: 20),
-                            padding: const EdgeInsets.all(10),
-                        ),
-                        onPressed: () {
-                            print('Button $index pressed');
-                        },
-                        child: Text(items[index], style: const TextStyle(color: AppColor.foreground)),
-                    );
-                }),
-            )
-        );
-    }
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: appTheme,
+        home: Scaffold(
+          body: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                return ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: AppColor.line,
+                    foregroundColor: AppColor.foreground,
+                    shadowColor: AppColor.foreground,
+                    elevation: 5,
+                    textStyle: const TextStyle(fontSize: 20),
+                    padding: const EdgeInsets.all(10),
+                  ),
+                  onPressed: () {
+                    print('Button $index pressed');
+                  },
+                  child: Text(items[index],
+                      style: const TextStyle(color: AppColor.foreground)),
+                );
+              }),
+        ));
+  }
 }
