3200e2f31e83bfb77c5f61be6d854548624c4acd
zahersunique <zaher@bandindustries.com>
2024-02-29 19:31:08 +0100

    added app colors file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'utils/app_colors.dart';
+
 
 void main() {
     runApp(const MyApp());
@@ -7,9 +9,19 @@ void main() {
 class MyApp extends StatelessWidget {
     const MyApp({super.key});
 
+    // This widget is the root of your application.
     @override
-    Widget build(BuildContext context) {
-        return const Text("hala rayis");
-
-    }
+        Widget build(BuildContext context) {
+            return const MaterialApp(
+                    home: Scaffold(
+                        backgroundColor: AppColor.backgroundColor,
+                        body: Center(
+                            child: Text(
+                                'Hala Rayyis', style: TextStyle(color: AppColor.foregroundColor, fontSize: 30.0)
+                                )
+                            )
+                        )
+                    );
+        }
 }
+
---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+    static const Color foregroundColor = Color(0xFF707A8C);
+    static const Color backgroundColor = Color(0xFF1F2430);
+    static const Color lineColor = Color(0xFF171B24);
+}
